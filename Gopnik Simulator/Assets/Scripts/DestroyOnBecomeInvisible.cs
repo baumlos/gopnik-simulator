ae9f8da94177142077ac844e908dd49d9a2e365e
JemLink <jana.hoffard@t-online.de>
2019-12-07 21:05:25 +0100

    Babushka and angry Gopnik added

---
+++ Gopnik Simulator/Assets/Scripts/DestroyOnBecomeInvisible.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnBecomeInvisible : MonoBehaviour
+{
+    public BabushkaPlayer player;
+    public bool captured = false;
+
+    private void Start()
+    {
+        player = FindObjectOfType<BabushkaPlayer>();
+    }
+
+
+    
+
+    private void OnBecameInvisible()
+    {
+        if (!captured)
+        {
+            player.DestroyVodka();
+            Destroy(this.gameObject);
+        }
+       
+
+
+    }
+}
---
+++ Gopnik Simulator/Assets/Scripts/DrawGizmos.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmos : MonoBehaviour
+{
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawCube(transform.position, );
+    //}
+}
