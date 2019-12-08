0a3e60d24c5db74a5776bb89609426e5a727ea8a
Anas <anastasiapome@gmail.com>
2019-12-08 08:04:23 +0100

    oberwelt

---
+++ Gopnik Simulator/Assets/Scripts/WalkAroundSprite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkAroundSprite : MonoBehaviour
+{
+    public Transform player;
+    public int minLayer;
+    public int maxLayer;
+    private SpriteRenderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player behind you
+        if(player.transform.position.z > transform.position.z)
+        {
+            rend.sortingOrder = maxLayer;
+        }
+        else
+        {
+            rend.sortingOrder = minLayer;
+        }
+          
+    }
+}
